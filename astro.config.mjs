05f83456fbacdb9dbfb9a34dab3ae4eccce43dff
刘晓 <liuxiaoabc@gmail.com>
2023-05-24 01:14:33 +0800

    feat: add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  base: "/CodingBlog",
   integrations: [
     tailwind({
       config: {
